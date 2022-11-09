3cc46e6ad1da19256435cf6234269dfdfe0123bb
Williams Samuel <samwill300@gmail.com>
2022-11-09 19:23:02 +0100

    add a under construction banner

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,28 @@
-import Hero from './Hero';
-import { Nav } from './Nav';
+import Warning from '@/assets/icons/Warning'
+import Whatsapp from '@/assets/icons/Whatsapp'
+import Hero from './Hero'
+import { Nav } from './Nav'
 
 export const Header = () => {
 	return (
-		<header className='bg-black text-gray-100 hero-bg'>
-			<Nav />
+		<>
+			{/* remeber to delete */}
+			<div className='flex items-center justify-between py-2 px-5 fixed top w-full bg-yellow-400 z-50'>
+				<p className='text-red-800 font-bold flex items-center gap-1'>
+					<Warning />
+					<span>Site still under construction</span>
+				</p>
+				<a
+					href='https://api.whatsapp.com/send?phone=2348131200424&text=Thank%20you%20for%20contacting%20DuraCoins%20%F0%9F%A4%A9.%20%20What%20do%20you%20want%20to%20trade%20with%20us%2C%20Giftcard%2C%20BTC%20or%20USDT%E2%9D%93'
+					className='uppercase text-xs flex items-center gap-1 font-bold text-gray-100 bg-green-600 hover:bg-green-800 py-2 px-4 rounded-md transition-all'>
+					<Whatsapp /> <span>Chat with us on WhatsApp</span>
+				</a>
+			</div>
+			<header className='bg-black text-gray-100 hero-bg'>
+				<Nav />
 
-			<Hero />
-		</header>
-	);
-};
+				<Hero />
+			</header>
+		</>
+	)
+}
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,12 +1,13 @@
-import Link from 'next/link';
+import MetaMask from '@/assets/logo/MetaMask'
+import Link from 'next/link'
 
 export const Nav = () => {
 	const navs = [
-		// {
-		// 	id: 1,
-		// 	name: 'about',
-		// 	link: '',
-		// },
+		{
+			id: 1,
+			name: 'dashboard',
+			link: '/dashboard',
+		},
 		{
 			id: 2,
 			name: 'log in',
@@ -17,11 +18,11 @@ export const Nav = () => {
 			name: 'sign up',
 			link: '/signup',
 		},
-	];
+	]
 	return (
 		// fixed top-3 left-1/2 -translate-x-1/2
 		<div className='container'>
-			<nav className='flex items-center justify-between w-full md:gap-12 text-gray-900 bg-gray-100 py-3 px-5 md:px-10 rounded-bl-xl rounded-br-xl shadow-duro-coins'>
+			<nav className='flex items-center justify-between w-full md:gap-12 text-gray-900 bg-gray-100 py-2 px-5 md:px-10 rounded-bl-xl rounded-br-xl shadow-duro-coins'>
 				<h1 className='font-black tracking-wide text-lg'>Dura-coins</h1>
 
 				<ul className='flex items-center gap-6'>
@@ -34,15 +35,16 @@ export const Nav = () => {
 							</Link>
 						</li>
 					))}
-					{/* <li>
-						<Link href='/dashboard'>
-							<a className='capitalize text-sm text-gray-700 hover:font-bold hovered-link'>
-								Dashboard
+					<li>
+						<Link href='#'>
+							<a className='uppercase text-xs flex items-center gap-1 font-bold text-gray-100 bg-gray-800 hover:bg-gray-900 py-2 px-4 rounded-md transition-all'>
+								<MetaMask />
+								<span>Sign in With ETH</span>
 							</a>
 						</Link>
-					</li> */}
+					</li>
 				</ul>
 			</nav>
 		</div>
-	);
-};
+	)
+}
